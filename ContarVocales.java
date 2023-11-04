9d31353e192232aac1f4bb086224c605801fc3b6
Gonzalo Carrascal <gcarrascalm@gmail.com>
2023-11-04 18:36:17 +0100

    Declarado metodo en ContarVocales.java

--- ContarVocales.java
+++ ContarVocales.java
@@ -1,2 +1,9 @@
 public class ContarVocales {
+	public static void main(String[] args) {
+		System.out.println(contarVocales("Hola Mundo"));
+	}
+
+	public static int contarVocales(String frase) {
+		return 0;
+	}
 }
