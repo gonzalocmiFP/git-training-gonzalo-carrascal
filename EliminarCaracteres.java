6856d2e1638e02a6ba63f677b1751fce8da8efe0
Gonzalo Carrascal <gcarrascalm@gmail.com>
2023-11-04 18:56:32 +0100

    Declarado metodo eliminar en EliminarCaracteres.java

--- EliminarCaracteres.java
+++ EliminarCaracteres.java
@@ -1,3 +1,16 @@
 public class EliminarCaracteres {
-	
+	public static void main(String[] args) {
+		System.out.println("Eliminar caracteres");
+		System.out.println(EliminarCaracteres.elimiar("Person"));
+                System.out.println(EliminarCaracteres.elimiar("Hello"));
+                System.out.println(EliminarCaracteres.elimiar("P"));
+                System.out.println(EliminarCaracteres.elimiar("Peter"));
+                System.out.println(EliminarCaracteres.elimiar(""));
+                System.out.println(EliminarCaracteres.elimiar(null));
+	}
+
+	/** Elimina la primera y ultima letra */
+	public static String eliminar(String str) {
+		return null;
+	}
 }
